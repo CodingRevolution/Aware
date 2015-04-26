01ac8c85c3be70683b00ad20e60323de3fa492ae
TheOnlyCreator <wassil.janssen@gmail.com>
2015-04-26 22:15:11 +0300

    Milestone: Start

---
+++ Main.lua
@@ -0,0 +1,16 @@
+--[[
+	Main Script for Aware
+	by CodingRevolution
+]]--
+
+
+--Variables--
+
+--Functions--
+
+--Code--
+
+term.setBackgroundColor(colors.black)
+term.setTextColor(colors.white)
+term.clear()
+term.setCursorPos(1,1)
