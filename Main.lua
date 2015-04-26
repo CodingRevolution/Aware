2b3282164cb338824d824d2cbf7809f17d4b9184
TheOnlyCreator <wassil.janssen@gmail.com>
2015-04-26 22:42:49 +0300

    Oh, gosh. Does not work

--- Main.lua
+++ Main.lua
@@ -14,3 +14,20 @@ term.setBackgroundColor(colors.black)
 term.setTextColor(colors.white)
 term.clear()
 term.setCursorPos(1,1)
+
+while true do
+	term.write("Input a valid sentence: ")
+	local input = read()
+	local header = {
+		input = textutils.urlEncode(input),
+		options = "none",
+		version= "0.1"
+	}
+	local httpObj, sec = http.get("http://www.naturalparsing.com/api/parse",header)
+	print(textutils.urlEncode(input))
+	print(sec)
+	for i,v in pairs(httpObj) do
+		print(i)
+	end
+	print(httpObj.read())
+end
